103d6ca08d023e0969ed0cd28a0090bcb11c59a2
JuHyeonPark <parkjh080@naver.com>
2023-11-20 22:57:04 +0900

    feat: 초기 셋팅
    초기 셋팅

---
+++ src/main/java/kr/co/fastcampus/fastcatch/FastcatchApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.fastcatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcatchApplication.class, args);
+	}
+
+}
