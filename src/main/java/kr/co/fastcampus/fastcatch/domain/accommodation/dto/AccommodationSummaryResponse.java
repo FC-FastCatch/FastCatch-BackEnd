3ac1d4327288e969a7556b5b60382287b99f51c2
JuHyeonPark <parkjh080@naver.com>
2023-11-21 19:10:23 +0900

    feat: accommodation 도메인 예시 작성
    accommodation 도메인 예시 작성

---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/accommodation/dto/AccommodationSummaryResponse.java
@@ -0,0 +1,4 @@
+package kr.co.fastcampus.fastcatch.domain.accommodation.dto;
+
+public class AccommodationSummaryResponse {
+}
---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/accommodation/entity/Accommodation.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.fastcatch.domain.accommodation.entity;
+
+import jakarta.persistence.Entity;
+import kr.co.fastcampus.fastcatch.common.baseentity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Accommodation extends BaseEntity {
+
+}
