a9f451bc58bb9516e92926a681f6c51796c22bb4
HyunA <vikim1210@naver.com>
2023-11-24 04:44:09 +0900

    refactor: cartItem 패키지를 cart로 변경

--- src/main/java/kr/co/fastcampus/fastcatch/domain/cart/entity/CartItem.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/cart/entity/CartItem.java
@@ -3,6 +3,7 @@ package kr.co.fastcampus.fastcatch.domain.cart.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,12 +38,12 @@ public class CartItem extends BaseEntity {
     @Column(nullable = false)
     private Integer price;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id")
     @JsonIgnore
     private Cart cart;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     @JsonIgnore
     private Room room;
