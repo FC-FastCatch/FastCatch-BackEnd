7ece6e9edf2accb3bfe18a2fbbec7cd5dfee7e66
JuHyeonPark <parkjh080@naver.com>
2023-11-25 23:35:22 +0900

    feat: RoomRepostitory 구현
    RoomRepostitory 구현

---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/accommodation/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package kr.co.fastcampus.fastcatch.domain.accommodation.repository;
+
+import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
