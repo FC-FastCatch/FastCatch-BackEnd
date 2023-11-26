1d080bf31359a2dc60001b0617d0deabc1786d71
JuHyeonPark <parkjh080@naver.com>
2023-11-26 21:48:23 +0900

    chore: 체크 스타일에 맞게 수정
    체크 스타일에 맞게 수정

--- src/main/java/kr/co/fastcampus/fastcatch/domain/accommodation/entity/Room.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/accommodation/entity/Room.java
@@ -1,8 +1,5 @@
 package kr.co.fastcampus.fastcatch.domain.accommodation.entity;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,6 +11,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import kr.co.fastcampus.fastcatch.common.baseentity.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
