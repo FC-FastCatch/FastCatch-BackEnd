0abcb2f4146020fdee2ed549685359c19d104ce6
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 21:32:02 +0900

    feat: MemberRepository 인터페이스 추가 및 JPA 설정, existsByEmail method 추가

---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package kr.co.fastcampus.fastcatch.domain.member.repository;
+
+import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+}
