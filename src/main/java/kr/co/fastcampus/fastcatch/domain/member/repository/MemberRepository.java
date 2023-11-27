99e72a86bec5e06138f1f4e0b9bc0a60f3ece0aa
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 23:12:10 +0900

    feat: MemberRepository에 findByEmail method 추가

--- src/main/java/kr/co/fastcampus/fastcatch/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/repository/MemberRepository.java
@@ -2,8 +2,11 @@ package kr.co.fastcampus.fastcatch.domain.member.repository;
 
 import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
