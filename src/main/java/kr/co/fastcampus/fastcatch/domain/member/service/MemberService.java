adc63c3d700d4c1b468cf92bd99a5d6110ce7999
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 23:11:13 +0900

    feat: MemberService에 로그인 관련 createLogin method 추가

--- src/main/java/kr/co/fastcampus/fastcatch/domain/member/service/MemberService.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/service/MemberService.java
@@ -2,8 +2,12 @@ package kr.co.fastcampus.fastcatch.domain.member.service;
 
 
 import kr.co.fastcampus.fastcatch.common.exception.DuplicateEmailException;
-import kr.co.fastcampus.fastcatch.domain.member.dto.MemberSignupRequest;
-import kr.co.fastcampus.fastcatch.domain.member.dto.MemberSignupResponse;
+import kr.co.fastcampus.fastcatch.common.exception.InvalidPasswordException;
+import kr.co.fastcampus.fastcatch.common.exception.MemberNotFoundException;
+import kr.co.fastcampus.fastcatch.domain.member.dto.request.MemberLoginRequest;
+import kr.co.fastcampus.fastcatch.domain.member.dto.response.MemberLoginResponse;
+import kr.co.fastcampus.fastcatch.domain.member.dto.request.MemberSignupRequest;
+import kr.co.fastcampus.fastcatch.domain.member.dto.response.MemberSignupResponse;
 import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
 import kr.co.fastcampus.fastcatch.domain.member.passwordencoder.BCryptPasswordEncoder;
 import kr.co.fastcampus.fastcatch.domain.member.repository.MemberRepository;
@@ -17,17 +21,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public MemberSignupResponse createMember(MemberSignupRequest request) {
-
         if (memberRepository.existsByEmail(request.email())) {
             throw new DuplicateEmailException();
         }
         Member member = memberRepository.save(request.toEntity(bCryptPasswordEncoder));
-
         return MemberSignupResponse.from(member);
+    }
+
+    @Transactional
+    public MemberLoginResponse createLogin(MemberLoginRequest request) {
+        Member member = memberRepository.findByEmail(request.email())
+            .orElseThrow(MemberNotFoundException::new);
+        if(bCryptPasswordEncoder.checkPassword(request.password(), member.getPassword())){
+            return MemberLoginResponse.from(member);
+        } else {
+            throw new InvalidPasswordException();
+        }
     }
 }
 
