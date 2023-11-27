2df2deebd0af2f748e8639725e7947ec96becffc
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 21:32:53 +0900

    feat: MemberService에 회원가입에 활용될 createMember method 추가

---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/service/MemberService.java
@@ -0,0 +1,37 @@
+package kr.co.fastcampus.fastcatch.domain.member.service;
+
+
+import kr.co.fastcampus.fastcatch.common.exception.DuplicateEmailException;
+import kr.co.fastcampus.fastcatch.domain.member.dto.MemberSignupRequest;
+import kr.co.fastcampus.fastcatch.domain.member.dto.MemberSignupResponse;
+import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
+import kr.co.fastcampus.fastcatch.domain.member.passwordencoder.BCryptPasswordEncoder;
+import kr.co.fastcampus.fastcatch.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public MemberSignupResponse createMember(MemberSignupRequest request) {
+
+        if (memberRepository.existsByEmail(request.email())) {
+            throw new DuplicateEmailException();
+        }
+        Member member = memberRepository.save(request.toEntity(bCryptPasswordEncoder));
+
+        return MemberSignupResponse.from(member);
+    }
+}
+
+
+
+
+
