52747e551d7557729bee5743e8893347514c548d
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 23:20:08 +0900

    feat: DuplicateEmailException 클래스 추가 및 설정

--- src/main/java/kr/co/fastcampus/fastcatch/common/exception/DuplicateEmailException.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/exception/DuplicateEmailException.java
@@ -2,7 +2,7 @@ package kr.co.fastcampus.fastcatch.common.exception;
 
 import kr.co.fastcampus.fastcatch.common.response.ErrorCode;
 
-public class DuplicateEmailException extends BaseException{
+public class DuplicateEmailException extends BaseException {
 
     public DuplicateEmailException() { super(ErrorCode.DUPLICATE_EMAIL.getErrorMsg()); }
 }
