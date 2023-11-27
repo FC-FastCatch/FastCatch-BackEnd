5031d114068031b5fe9746074c167482cc331c58
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-27 21:30:46 +0900

    feat: BCryptPasswordEncoder 클래스 추가 및 hashPassword / checkPassword method 추가

---
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/passwordencoder/BCryptPasswordEncoder.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.fastcatch.domain.member.passwordencoder;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordEncoder {
+    public String hashPassword(String rawPassword){ return BCrypt.hashpw(rawPassword, BCrypt.gensalt()); }
+
+    public boolean checkPassword(String memberPassword, String hashedPassword) {
+        return BCrypt.checkpw(memberPassword, hashedPassword);
+    }
+}
