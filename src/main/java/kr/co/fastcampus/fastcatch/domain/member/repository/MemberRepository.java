34d61dc7e6c8019c9602af4f75941a75d12d3cca
JuHyeonPark <parkjh080@naver.com>
2023-11-28 22:14:13 +0900

    fix: 닉네임 조회 API 명세서에 맞게 수정
    닉네임 조회 API 명세서에 맞게 수정

--- src/main/java/kr/co/fastcampus/fastcatch/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/repository/MemberRepository.java
@@ -10,6 +10,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByNickname(String nickname);
 
-    Optional<Member> findByNickname(String nickname);
-
 }
