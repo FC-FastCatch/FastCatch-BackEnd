bf90292fc80d48349beb61a7521e9de1d0f10502
dldmldlsy <dldmldlsy@naver.com>
2023-11-28 16:43:00 +0900

    feat: 회원가입 시, 닉네임 중복 검증 로직 추가 구현

--- src/main/java/kr/co/fastcampus/fastcatch/domain/member/service/MemberService.java
+++ src/main/java/kr/co/fastcampus/fastcatch/domain/member/service/MemberService.java
@@ -1,7 +1,8 @@
 package kr.co.fastcampus.fastcatch.domain.member.service;
 
 
-import kr.co.fastcampus.fastcatch.common.exception.DuplicateEmailException;
+import kr.co.fastcampus.fastcatch.common.exception.DuplicatedEmailException;
+import kr.co.fastcampus.fastcatch.common.exception.DuplicatedNicknameException;
 import kr.co.fastcampus.fastcatch.domain.member.dto.request.MemberSignupRequest;
 import kr.co.fastcampus.fastcatch.domain.member.dto.response.MemberSignupResponse;
 import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
@@ -24,7 +25,10 @@ public class MemberService {
     public MemberSignupResponse createMember(MemberSignupRequest request) {
 
         if (memberRepository.existsByEmail(request.email())) {
-            throw new DuplicateEmailException();
+            throw new DuplicatedEmailException();
+        }
+        if (memberRepository.existsByNickname(request.nickname())) {
+            throw new DuplicatedNicknameException();
         }
         Member member = memberRepository.save(request.toEntity(passwordEncoder));
 
