b27b355108b912c00f220fad08f9a4cd00eef57c
WhiteSoup <129931655+fancytomorrow@users.noreply.github.com>
2023-11-28 12:47:46 +0900

    style: DuplicateEmailException 에 구글 컨벤션 적용

--- src/main/java/kr/co/fastcampus/fastcatch/common/exception/DuplicateEmailException.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/exception/DuplicateEmailException.java
@@ -2,7 +2,9 @@ package kr.co.fastcampus.fastcatch.common.exception;
 
 import kr.co.fastcampus.fastcatch.common.response.ErrorCode;
 
-public class DuplicateEmailException extends BaseException{
+public class DuplicateEmailException extends BaseException {
 
-    public DuplicateEmailException() { super(ErrorCode.DUPLICATE_EMAIL.getErrorMsg()); }
+    public DuplicateEmailException() {
+        super(ErrorCode.DUPLICATE_EMAIL.getErrorMsg());
+    }
 }
