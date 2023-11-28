288f9372e9772dd8378ab2ee38e0e1788df58911
HyunA <vikim1210@naver.com>
2023-11-28 14:29:27 +0900

    refactor: 장바구니 에러 메시지 변경

--- src/main/java/kr/co/fastcampus/fastcatch/common/response/ErrorCode.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/response/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     ACCOMMODATION_NOT_FOUND("존재하지 않는 숙박 장소입니다."),
     ROOM_NOT_FOUND("존재하지 않는 객실입니다."),
     DUPLICATED_REQUEST("중복된 요청입니다."),
-    CART_NOT_FOUND("해당 하는 카트가 없습니다."),
-    CART_ITEM_NOT_FOUND("해당 하는 카트 아이템이 없습니다."),
+    CART_NOT_FOUND("해당 하는 장바구니가 없습니다."),
+    CART_ITEM_NOT_FOUND("해당 하는 장바구니 아이템이 없습니다."),
     PAST_DATE("현재 날짜 보다 과거의 날짜는 예약 할 수 없습니다."),
     INVALID_DATE_RANGE("시작 날짜가 종료 날짜 보다 늦습니다."),
     INVALID_HEAD_RANGE("해당 인원 수로 예약할 수 없습니다.")
