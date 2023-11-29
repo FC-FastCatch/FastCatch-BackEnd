29cb4c8cb86e188c77a1bae50f6675ee4018998e
JuHyeonPark <parkjh080@naver.com>
2023-11-29 17:39:48 +0900

    test: 테스트 코드 틀 작성
    테스트 코드 틀 작성

---
+++ src/test/java/kr/co/fastcampus/fastcatch/domain/accommodation/controller/AccommodationControllerTest.java
@@ -0,0 +1,7 @@
+package kr.co.fastcampus.fastcatch.domain.accommodation.controller;
+
+import kr.co.fastcampus.fastcatch.common.ApiTest;
+
+public class AccommodationControllerTest extends ApiTest {
+
+}
