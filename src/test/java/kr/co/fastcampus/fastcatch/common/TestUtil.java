4398d85e1318f2286bac1b37979f0a8c41c56575
dldmldlsy <dldmldlsy@naver.com>
2023-11-30 02:17:44 +0900

    test: 주문 Service 테스트코드 구현

--- src/test/java/kr/co/fastcampus/fastcatch/common/TestUtil.java
+++ src/test/java/kr/co/fastcampus/fastcatch/common/TestUtil.java
@@ -1,12 +1,21 @@
 package kr.co.fastcampus.fastcatch.common;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Accommodation;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.AccommodationOption;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Category;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Region;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Room;
 import kr.co.fastcampus.fastcatch.domain.accommodation.entity.RoomOption;
+import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderByCartRequest;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderItemRequest;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderRequest;
+import kr.co.fastcampus.fastcatch.domain.order.entity.Order;
+import kr.co.fastcampus.fastcatch.domain.order.entity.OrderItem;
+import kr.co.fastcampus.fastcatch.domain.order.entity.OrderStatus;
 
 public class TestUtil {
 
@@ -65,5 +74,37 @@ public class TestUtil {
             .canSmoking(false)
             .hasNetflix(true)
             .build();
+    }
+
+    public static Member createMember() {
+        return Member.builder().memberId(1L).email("test@naver.com")
+            .password("test1234").name("test").nickname("test")
+            .birthday(LocalDate.of(2000, 0, 1)).build();
+    }
+
+    public static Order createOrder(Member member) {
+        return Order.builder().orderId(1L).member(member).
+            reservationPersonName("test").reservationPhoneNumber("01012341234")
+            .totalPrice(80000).orderStatus(OrderStatus.COMPLETED).build();
+    }
+
+    public static OrderItem createOrderItem(Room room, Order order) {
+       return OrderItem.builder().orderItemId(1L).order(order).room(room).startDate(LocalDate.of(2023, 11, 29))
+            .endDate(LocalDate.of(2023, 11, 30)).headCount(2).price(80000).build();
+    }
+
+    public static OrderRequest createOrderRequest(List<OrderItemRequest> orderItemRequestList) {
+        return new OrderRequest(
+            true, "test", "01012341234", 80000, orderItemRequestList);
+    }
+
+    public static OrderItemRequest createOrderItemRequest() {
+        return new OrderItemRequest(1L, LocalDate.of(2023, 11, 29),
+            LocalDate.of(2023, 11, 30), 2, 80000);
+    }
+
+    public static OrderByCartRequest createOrderByCartRequest(List<Long> cartIdList) {
+        return new OrderByCartRequest(true, "test", "01012341234",
+            80000, cartIdList);
     }
 }
---
+++ src/test/java/kr/co/fastcampus/fastcatch/domain/order/service/OrderServiceTest.java
@@ -0,0 +1,258 @@
+package kr.co.fastcampus.fastcatch.domain.order.service;
+
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createAccommodation;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createMember;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createOrder;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createOrderByCartRequest;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createOrderItem;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createOrderItemRequest;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createOrderRequest;
+import static kr.co.fastcampus.fastcatch.common.TestUtil.createRoom;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.List;
+import kr.co.fastcampus.fastcatch.domain.accommodation.entity.Room;
+import kr.co.fastcampus.fastcatch.domain.cart.entity.CartItem;
+import kr.co.fastcampus.fastcatch.domain.cart.service.CartService;
+import kr.co.fastcampus.fastcatch.domain.member.entity.Member;
+import kr.co.fastcampus.fastcatch.domain.member.service.MemberService;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderByCartRequest;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderItemRequest;
+import kr.co.fastcampus.fastcatch.domain.order.dto.request.OrderRequest;
+import kr.co.fastcampus.fastcatch.domain.order.dto.response.OrderItemResponse;
+import kr.co.fastcampus.fastcatch.domain.order.dto.response.OrderPageResponse;
+import kr.co.fastcampus.fastcatch.domain.order.dto.response.OrderResponse;
+import kr.co.fastcampus.fastcatch.domain.order.dto.response.OrdersResponse;
+import kr.co.fastcampus.fastcatch.domain.order.entity.Order;
+import kr.co.fastcampus.fastcatch.domain.order.entity.OrderItem;
+import kr.co.fastcampus.fastcatch.domain.order.entity.OrderRecord;
+import kr.co.fastcampus.fastcatch.domain.order.entity.OrderStatus;
+import kr.co.fastcampus.fastcatch.domain.order.repository.OrderItemRepository;
+import kr.co.fastcampus.fastcatch.domain.order.repository.OrderRecordRepository;
+import kr.co.fastcampus.fastcatch.domain.order.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ExtendWith(MockitoExtension.class)
+class OrderServiceTest {
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private OrderItemRepository orderItemRepository;
+
+    @Mock
+    private OrderRecordRepository orderRecordRepository;
+
+    @Mock
+    private CartService cartService;
+
+    @Mock
+    private MemberService memberService;
+
+
+    @Nested
+    @DisplayName("createOrder")
+    class Context_createOrder {
+
+
+        @Test
+        @DisplayName("주문을 생성할 수 있다.")
+        void _willSuccess() {
+            //given
+            Long memberId = 1L;
+            List<OrderItemRequest> orderItemRequestList = List.of(createOrderItemRequest());
+            OrderRequest orderRequest = createOrderRequest(orderItemRequestList);
+
+            Member member = createMember();
+            Order order = createOrder(member);
+
+            given(memberService.findMemberById(any(Long.TYPE))).willReturn(any(Member.class));
+            given(orderRequest.toEntity(any(Member.class))).willReturn(any(Order.class));
+            doNothing().when(orderService)
+                .createOrderItem(any(OrderItemRequest.class), any(Order.class));
+            doNothing().when(orderService).checkOrderRecord(any(OrderItemRequest.class));
+            given(orderRepository.save(any(Order.class))).willReturn(any(Order.class));
+            doNothing().when(orderService).createOrderRecord(any(OrderItem.class));
+
+            //when
+            orderService.createOrder(memberId, orderRequest);
+
+            //then
+            verify(orderRepository, times(1)).save(any(Order.class));
+            verify(orderItemRepository, times(orderRequest.orderItems().size())).save(
+                any(OrderItem.class));
+            verify(orderRecordRepository, times(order.getOrderItems().size())).save(
+                any(OrderRecord.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("createOrderByCart()는 ")
+    class Context_createOrderByCart {
+
+        @Test
+        @DisplayName("장바구니를 통해 주문을 생성할 수 있다.")
+        void _willSuccess() {
+            //given
+            Long memberId = 1L;
+            OrderByCartRequest orderByCartRequest = createOrderByCartRequest(List.of(1L));
+            Member member = createMember();
+            Order order = createOrder(member);
+
+            given(memberService.findMemberById(any(Long.TYPE))).willReturn(any(Member.class));
+            given(cartService.findCartItemById(any(Long.TYPE))).willReturn(any(CartItem.class));
+            given(OrderItemRequest.fromCartItem(any(CartItem.class))).willReturn(
+                any(OrderItemRequest.class));
+            doNothing().when(orderService)
+                .createOrderItem(any(OrderItemRequest.class), any(Order.class));
+            doNothing().when(orderService).checkOrderRecord(any(OrderItemRequest.class));
+            doNothing().when(cartService.deleteCartItem(any(Long.class), any(Long.class)));
+            given(orderRepository.save(any(Order.class))).willReturn(any(Order.class));
+            doNothing().when(orderService).createOrderRecord(any(OrderItem.class));
+
+            //when
+            orderService.createOrderByCart(memberId, orderByCartRequest);
+
+            //then
+            verify(orderRepository, times(1)).save(any(Order.class));
+            verify(orderItemRepository, times(orderByCartRequest.cartItemIds().size())).save(
+                any(OrderItem.class));
+            verify(orderRecordRepository, times(order.getOrderItems().size())).save(
+                any(OrderRecord.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("findOrders()는 ")
+    class Context_findOrders {
+
+        @Test
+        @DisplayName("주문 목록을 조회할 수 있다.")
+        void _willSuccess() {
+            //given
+            Long memberId = 1L;
+            Pageable pageable = PageRequest.of(0, 3);
+
+            Room room = createRoom(createAccommodation());
+            Order order1 = createOrder(createMember());
+            Order order2 = createOrder(createMember());
+            Order order3 = createOrder(createMember());
+            List<OrderItemResponse> orderItemResponseList1 = List.of(
+                OrderItemResponse.from(createOrderItem(room, order1)));
+            List<OrderItemResponse> orderItemResponseList2 = List.of(
+                OrderItemResponse.from(createOrderItem(room, order2)));
+            List<OrderItemResponse> orderItemResponseList3 = List.of(
+                OrderItemResponse.from(createOrderItem(room, order3)));
+            OrderResponse orderResponseReserved = OrderResponse.from(order1, orderItemResponseList1,
+                "reserved");
+            OrderResponse orderResponseUsed = OrderResponse.from(order2, orderItemResponseList2,
+                "used");
+            OrderResponse orderResponseCanceled = OrderResponse.from(order3, orderItemResponseList3,
+                "canceled");
+
+            when(orderService.findOrdersByStatus(any(Long.TYPE), anyString(), any(Pageable.class)))
+                .thenReturn(OrderPageResponse.from(new PageImpl<>(List.of(orderResponseReserved)),
+                    "reserved"))
+                .thenReturn(
+                    OrderPageResponse.from(new PageImpl<>(List.of(orderResponseUsed)), "used"))
+                .thenReturn(OrderPageResponse.from(new PageImpl<>(List.of(orderResponseCanceled)),
+                    "canceled"));
+
+            //when
+            OrdersResponse result = orderService.findOrders(memberId, pageable);
+
+            //then
+            assertNotNull(result);
+            assertThat(result.orders().size()).isEqualTo(3);
+            verify(orderService, times(3)).findOrdersByStatus(any(Long.TYPE), anyString(),
+                any(Pageable.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("findOrdersByStatus()는 ")
+    class Context_findOrdersByStatus {
+
+        @Test
+        @DisplayName("특정 주문 상태의 주문목록을 조회할 수 있다.")
+        void _willSuccess() {
+            //given
+            Pageable pageable = PageRequest.of(1, 3);
+            String status = "reserved";
+            Member member = createMember();
+            Order order = createOrder(member);
+            OrderItem orderItem = createOrderItem(createRoom(createAccommodation()), order);
+            Page<Order> orders = new PageImpl<>(List.of(order));
+            OrderResponse orderResponseReserved = OrderResponse.from(order,
+                List.of(OrderItemResponse.from(orderItem)), status);
+
+            given(memberService.findMemberById(any(Long.TYPE))).willReturn(any(Member.class));
+            given(orderRepository.findOrdersReserved(member, OrderStatus.COMPLETED, LocalDate.now(),
+                pageable)).willReturn(orders);
+
+            //when
+            OrderPageResponse result = orderService.findOrdersByStatus(member.getMemberId(),
+                "reserved", pageable);
+
+            //then
+            assertNotNull(result);
+            assertThat(result.status()).isEqualTo("reserved");
+            assertThat(result.pageNum()).isEqualTo(1);
+            assertThat(result.pageSize()).isEqualTo(3);
+            assertThat(result.orderResponses()).isEqualTo(List.of(orderResponseReserved));
+            verify(orderRepository, times(1)).findOrdersReserved(any(Member.class),
+                any(OrderStatus.class), any(LocalDate.class), any(Pageable.class));
+        }
+    }
+
+    @Nested
+    @DisplayName("deleteOrder()는 ")
+    class Context_deleteOrder {
+
+        @Test
+        @DisplayName("주문을 취소할 수 있다.")
+        void _willSuccess() {
+            //given
+            Long memberId = 1L;
+            Long orderId = 1L;
+
+            Order order = createOrder(createMember());
+            order.setOrderCanceled();
+
+            given(orderRepository.save(any(Order.class))).willReturn(order);
+            doNothing().when(orderRecordRepository).deleteByOrder(any(Order.class));
+
+            //when
+            orderService.deleteOrder(memberId, orderId);
+
+            //then
+            assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.CANCELED);
+            verify(orderRepository, times(1)).save(any(Order.class));
+        }
+    }
+}
