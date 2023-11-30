59c51cb437a450a128a0c4c7198d7d96d6a4c3d7
JuHyeonPark <parkjh080@naver.com>
2023-11-30 20:23:03 +0900

    fix: cors 설정 변경
    cors 설정 변경

--- src/main/java/kr/co/fastcampus/fastcatch/common/config/WebConfig.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/config/WebConfig.java
@@ -9,14 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-//                "http://localhost:8080", "http://localhost:8081",
-//                "https://fast-catch-front-end-umh7.vercel.app", "http://localhost:5173",
-//                "http://58.127.37.16",
-//                "http://localhost:5174",
-//                "http://localhost:5175",
-//                "http://localhost:5176",
-//                "http://localhost:5177")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization")
             .allowCredentials(true)
