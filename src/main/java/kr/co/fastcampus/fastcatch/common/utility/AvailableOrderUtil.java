3d9eb036a8e4dee2570bfcefa6e3106dfa3f526f
dldmldlsy <dldmldlsy@naver.com>
2023-11-30 12:01:15 +0900

    refactor: 에러코드 네이밍 변경
    INVALID_HEAD_RANGE -> INVALID_HEAD_COUNT_RANGE

--- src/main/java/kr/co/fastcampus/fastcatch/common/utility/AvailableOrderUtil.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/utility/AvailableOrderUtil.java
@@ -2,10 +2,11 @@ package kr.co.fastcampus.fastcatch.common.utility;
 
 import java.time.LocalDate;
 import kr.co.fastcampus.fastcatch.common.exception.InvalidDateRangeException;
-import kr.co.fastcampus.fastcatch.common.exception.InvalidHeadRangeException;
+import kr.co.fastcampus.fastcatch.common.exception.InvalidHeadCountRangeException;
 import kr.co.fastcampus.fastcatch.common.exception.PastDateException;
 
 public class AvailableOrderUtil {
+
     public static void validateDate(LocalDate startDate, LocalDate endDate) {
         if (startDate.isBefore(LocalDate.now())) {
             throw new PastDateException();
@@ -18,7 +19,7 @@ public class AvailableOrderUtil {
     public static void validateHeadCount(
         int requestHeadCount, int roomBaseHeadCount, int roomMaxHeadcount) {
         if (requestHeadCount < roomBaseHeadCount || requestHeadCount > roomMaxHeadcount) {
-            throw new InvalidHeadRangeException();
+            throw new InvalidHeadCountRangeException();
         }
     }
 }
