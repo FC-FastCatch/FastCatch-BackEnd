13270e2fbb7e23694a6fca6b66755f08a92a22be
HyunA <vikim1210@naver.com>
2023-12-01 07:55:36 +0900

    feat: 오픈 api 완성 및 테스트 코드에 사용 예시 추가

--- src/main/java/kr/co/fastcampus/fastcatch/common/utility/api/OpenApiUtil.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/utility/api/OpenApiUtil.java
@@ -1,19 +1,20 @@
 package kr.co.fastcampus.fastcatch.common.utility.api;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import kr.co.fastcampus.fastcatch.common.utility.api.dto.response.OpenApiResponseDto;
-import kr.co.fastcampus.fastcatch.common.utility.api.dto.response.OpenApiResponseListDto;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 public class OpenApiUtil {
-    private static String baseUrl;
-    private static String serviceKey;
+    private static String baseUrl = "http://openapi.seoul.go.kr:8088";
+    private static String serviceKey = "5756515a6a76696b38347645497154";
 
     @Value("${spring.open-api.base-url}")
     private void setBaseUrl(String baseUrl) {
@@ -25,30 +26,30 @@ public class OpenApiUtil {
         OpenApiUtil.serviceKey = serviceKey;
     }
 
-    public static List<OpenApiResponseDto> requestOpenApi() {
-        URI uri = uriBuilder();
-
+    public static List<OpenApiResponseDto> requestOpenApi() throws ParseException {
+        URI uri = uriBuilder(1, 100);
         RestTemplate restTemplate = new RestTemplate();
+        String jsonString = restTemplate.getForObject(uri, String.class);
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        JSONObject jsonSebcHotelListKor = (JSONObject) jsonObject.get("SebcHotelListKor");
+        JSONArray jsonRowList = (JSONArray) jsonSebcHotelListKor.get("row");
 
-        return Optional.ofNullable(
-                restTemplate.exchange(uri, HttpMethod.GET, null, OpenApiResponseListDto.class)
-                    .getBody()
-            )
-            .get().item()
-            .stream().map(OpenApiResponseDto::from)
-            .toList();
+        List<OpenApiResponseDto> openApiResponseDtoList = new ArrayList<>();
+
+        for (Object object : jsonRowList) {
+            JSONObject row = (JSONObject) object;
+            openApiResponseDtoList.add(OpenApiResponseDto.fromJson(row));
+        }
+
+        return openApiResponseDtoList;
+
     }
 
-    private static URI uriBuilder() {
-        return UriComponentsBuilder.fromHttpUrl(baseUrl)
-            .queryParam("numOfRows", 100)
-            .queryParam("pageNo", 1)
-            .queryParam("MobileOS", "ETC")
-            .queryParam("MobileApp", "FastCatch")
-            .queryParam("_type", "json")
-            .queryParam("listYN", "Y")
-            .queryParam("serviceKey", serviceKey)
-            .build()
+    private static URI uriBuilder(int page, int count) {
+        return UriComponentsBuilder.fromUriString(baseUrl + "/" + serviceKey)
+            .path("/json/SebcHotelListKor/{page}/{count}")
+            .buildAndExpand(page, count)
             .toUri();
     }
 
--- src/main/java/kr/co/fastcampus/fastcatch/common/utility/api/dto/response/OpenApiResponseDto.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/utility/api/dto/response/OpenApiResponseDto.java
@@ -1,32 +1,23 @@
 package kr.co.fastcampus.fastcatch.common.utility.api.dto.response;
 
 import lombok.Builder;
+import org.json.simple.JSONObject;
 
 @Builder
 public record OpenApiResponseDto(
-    String addr1,
-    String areacode,
-    String firstimage,
-    String mapx,
-    String mapy,
-    String lot,
-    String lat,
-    String tel,
-    String title
+    String mainKey,
+    String category,
+    String name,
+    String address
 ) {
-    public static OpenApiResponseDto from(
-        OpenApiResponseDto openApiResponseDto
-    ) {
-        return OpenApiResponseDto.builder()
-            .addr1(openApiResponseDto.addr1)
-            .areacode(openApiResponseDto.areacode)
-            .firstimage(openApiResponseDto.firstimage)
-            .mapx(openApiResponseDto.mapx)
-            .mapy(openApiResponseDto.mapy)
-            .lot(openApiResponseDto.lot)
-            .lat(openApiResponseDto.lat)
-            .tel(openApiResponseDto.tel)
-            .title(openApiResponseDto.title)
+    public static OpenApiResponseDto fromJson(JSONObject row) {
+        return OpenApiResponseDto
+            .builder()
+            .mainKey((String) row.get("MAIN_KEY"))
+            .category((String) row.get("CATE3_NAME"))
+            .name((String) row.get("NAME_KOR"))
+            .address((String) row.get("H_KOR_CITY") + " " + row.get("H_KOR_GU") + " " +
+                row.get("H_KOR_DONG"))
             .build();
     }
 }
