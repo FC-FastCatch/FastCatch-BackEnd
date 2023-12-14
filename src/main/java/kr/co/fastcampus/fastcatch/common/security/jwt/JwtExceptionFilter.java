575efbf73008333b537cf51ee578dc7a88c0b886
dldmldlsy <dldmldlsy@naver.com>
2023-12-14 12:35:53 +0900

    refactor: 체크스타일 수정

--- src/main/java/kr/co/fastcampus/fastcatch/common/security/jwt/JwtExceptionFilter.java
+++ src/main/java/kr/co/fastcampus/fastcatch/common/security/jwt/JwtExceptionFilter.java
@@ -9,17 +9,22 @@ import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws IOException, ServletException {
         try {
             filterChain.doFilter(request, response);
         } catch (JwtException e) {
             setErrorResponse(HttpStatus.UNAUTHORIZED, response, e);
         }
     }
-    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable e) throws IOException {
+
+    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable e)
+        throws IOException {
         response.setStatus(status.value());
         response.setContentType("application/json; charset=UTF-8");
         response.getWriter().write(
